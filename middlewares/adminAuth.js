b01ee94efd6c04bd77bb612448807979d7bd4b37
adhipradhana <adhi.pradana.pradana@gmail.com>
2019-01-28 17:19:26 +0700

    change admin auth

--- middlewares/adminAuth.js
+++ middlewares/adminAuth.js
@@ -1,13 +1,37 @@
-var authenticateAdmin = (req, res, next) => {
-    let payload = req.decoded;
+const jwt = require('jsonwebtoken');
+const dotenv = require('dotenv');
+const fs = require('fs');
 
-    if (payload.admin) {
-        next();
-    } else {
-        return res.json({
-            status: "failed",
-            message: "You're not authorized"
+const vars = dotenv.config();
+if (vars.error) {
+  throw vars.error
+}
+
+const jwtKey = fs.readFileSync(process.env.JWT_PATH, 'utf8');
+
+var authenticateAdmin = (req, res, next) => {
+    console.log(req.cookies);
+    let token = req.cookies.jwtToken || req.headers['x-access-token'] || req.headers['authorization'];
+
+    if (token) {
+        if (token.startsWith('Bearer ')) {
+            // Remove Bearer from string
+            token = token.slice(7, token.length);
+        }
+
+        jwt.verify(token, jwtKey, (err, decoded) => {
+            if (err) {
+                return res.render('login.njk');
+            } else {
+                if (decoded.admin) {
+                    next();
+                } else {
+                    return res.render('login.njk');
+                }
+            }
         });
+    } else {
+        return res.render('login.njk');
     }
 }
 
